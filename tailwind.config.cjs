ca5477d1c25bc07e48aa3a25ec2d1e1c13c5333a
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2022-11-21 08:36:51 +0200

    initial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			'rose-wood': '#5B0017',
-			'white': '#FFFFFF',
-			'sheen-gold': '#CC9D2F',
-			'black': '#0000',
-			'space-cadet': '#243054',
-			'banana-mania': '#FFE9B6',
-			'gray-light': '#d3dce6',
-		  },
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "rose-wood": "#5B0017",
+      white: "#FFFFFF",
+      "sheen-gold": "#CC9D2F",
+      black: "#0000",
+      "space-cadet": "#243054",
+      "banana-mania": "#FFE9B6",
+      "gray-light": "#d3dce6",
+    },
+    extend: {
+      //   fontFamily: {
+      //     monospace: ["Xanh Mono", ...defaultTheme.fontFamily.mono],
+      //   },
+    },
+  },
+  plugins: [],
+};
